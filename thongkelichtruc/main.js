e720e640c10595ab07f273e82916f38bce065cf7
nghia <capnguyenhieunghia@gmail.com>
2024-06-10 19:46:06 +0700

    up

--- thongkelichtruc/main.js
+++ thongkelichtruc/main.js
@@ -90,3 +90,23 @@ const totalHours = totalMilliseconds / (1000 * 60 * 60);
 
 return totalHours.toFixed(2);
 }
+// quay lại đầu trang
+// Get the button
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+scrollToTopBtn.addEventListener("click", function () {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
