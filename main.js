e1680314fd695b6f45af5fc046c83c2484458a27
nghia <capnguyenhieunghia@gmail.com>
2024-06-13 18:27:07 +0700

    uP

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
+// Hàm lấy lời chào dựa trên thời gian trong ngày
 function getGreeting() {
     const currentDate = new Date();
     const hours = currentDate.getHours();
-
     if (hours >= 5 && hours < 12) {
         return "Chào buổi sáng!";
     } else if (hours >= 12 && hours < 18) {
@@ -11,35 +11,29 @@ function getGreeting() {
     }
 }
 
-function getDate() {
-    const currentDate = new Date();
-    const day = currentDate.getDate();
-    const month = currentDate.getMonth() + 1;
-    const year = currentDate.getFullYear();
-
-    return ` ${day}/${month}/${year}`;
-}
-
-function updateTime() {
+// Hàm cập nhật background dựa trên thời gian trong ngày
+function updateBackground() {
     const currentDate = new Date();
     const hours = currentDate.getHours();
-    const minutes = currentDate.getMinutes();
-    const seconds = currentDate.getSeconds();
 
-    const timeString = `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-    document.getElementById('time').textContent = timeString;
+    let backgroundClass;
+    if (hours >= 5 && hours < 12) {
+        backgroundClass = 'morning-background';
+    } else if (hours >= 12 && hours < 18) {
+        backgroundClass = 'afternoon-background';
+    } else {
+        backgroundClass = 'evening-background';
+    }
+
+    document.body.className = backgroundClass;
 }
 
+// Hàm cập nhật lời chào và background
 function updateGreeting() {
     const greeting = getGreeting();
     document.getElementById('greeting').textContent = greeting;
+    updateBackground();
 }
 
-function updateDate() {
-    const dateString = getDate();
-    document.getElementById('date').textContent = dateString;
-}
-
-setInterval(updateTime, 1000);
+// Gọi hàm updateGreeting() mỗi giây để cập nhật lời chào và background
 setInterval(updateGreeting, 1000);
-setInterval(updateDate, 1000);
